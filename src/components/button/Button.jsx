8183714d1a17191b202a4560f98ea47dac3c3934
BennettTheTiger <bas9386@g.rit.edu>
2021-08-01 22:20:57 -0400

    setup lint

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -4,24 +4,28 @@ import PropType from 'prop-types';
 import './button.scss';
 
 const Button = (props) => {
-    const {classes,  onClick} = props;
-    return (<button 
-        className={`${classes} clue-btn`}
-        onClick={onClick}
-    >{props.value}
-    </button>)
-}
+  const { classes, onClick, value } = props;
+  return (
+    <button
+      type="button"
+      className={`${classes} clue-btn`}
+      onClick={onClick}
+    >
+      {value}
+    </button>
+  );
+};
 
 Button.propTypes = {
-    value: PropType.string,
-    onClick: PropType.func,
-    classes: PropType.string
-}
+  value: PropType.string,
+  onClick: PropType.func,
+  classes: PropType.string,
+};
 
 Button.defaultProps = {
-    value: 'None',
-    classes: '',
-    onClick: () => {}
-}
+  value: 'None',
+  classes: '',
+  onClick: () => {},
+};
 
 export default Button;
--- src/components/entity/Enitity.jsx
+++ src/components/entity/Enitity.jsx
@@ -1,53 +1,56 @@
 import React from 'react';
 import cn from 'classnames';
 import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import Button from '../button';
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
 import interactions from '../../app/redux/actions/interactions';
 
 import './entity.scss';
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators(interactions, dispatch)
+  return bindActionCreators(interactions, dispatch);
 }
 
 function mapStateToProps(state) {
-    return { allEntities: state.gameState }
+  return { allEntities: state.gameState };
 }
 
 const Entity = (props) => {
-    const { name, toggleElegible, allEntities } = props;
-    const entityData = allEntities[name];
+  const { name, toggleElegible, allEntities } = props;
+  const entityData = allEntities[name];
 
-    function toggleElegibility() {
-        return toggleElegible(name);
-    }
+  function toggleElegibility() {
+    return toggleElegible(name);
+  }
 
-    return (
-        <div className='entity-item'>
-            <div className={cn('entity-name', {
-                elegible: entityData.isElegible
-                })}>
-                {name}
-            </div>
-            <Button 
-                classes='elegible-btn' 
-                value='Elegible' 
-                onClick={toggleElegibility}
-            />
-        </div>
-    )
-}
+  return (
+    <div className="entity-item">
+      <div className={cn('entity-name', {
+        elegible: entityData.isElegible,
+      })}
+      >
+        {name}
+      </div>
+      <Button
+        classes="elegible-btn"
+        value="Elegible"
+        onClick={toggleElegibility}
+      />
+    </div>
+  );
+};
 
 Entity.propTypes = {
-    name: PropTypes.string,
-    toggleElegible: PropTypes.func,
-    allEntities: PropTypes.array
-}
+  name: PropTypes.string,
+  toggleElegible: PropTypes.func,
+  allEntities: PropTypes.arrayOf,
+};
 
 Entity.defaultProps = {
-    name: ''
-}
+  name: '',
+  toggleElegible: () => {},
+  allEntities: [],
+};
 
-export default  connect(mapStateToProps, mapDispatchToProps)(Entity);
+export default connect(mapStateToProps, mapDispatchToProps)(Entity);
